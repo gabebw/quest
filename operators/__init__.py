e26fe951a9c7b85e5abaf214680449376365383d
Gabe Berke-Williams <gbw@brandeis.edu>
2010-10-27 11:57:57 -0400

    Allow calling operator functions like quest.operators.rollup()

---
+++ operators/__init__.py
@@ -0,0 +1,7 @@
+# Enable calling operator functions "directly", e.g. quest.operators.rollup()
+from rollup import rollup
+from drilldown import drilldown
+from relax import relax
+from narrow import narrow
+from relate import relate
+from store import store
