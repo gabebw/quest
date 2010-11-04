54af791b95a8f4e4bc4b1532ff8d3e47f39b09c4
Gabe Berke-Williams <gbw@brandeis.edu>
2010-11-04 10:58:18 -0400

    engine.engine_instance isn't a thing

--- repl/prompt.py
+++ repl/prompt.py
@@ -33,7 +33,7 @@ class Prompt:
         """Calls init_history()."""
         self.banner = banner
         self.init_history(histfile)
-        self.engine = quest.engine.engine_instance
+        self.engine = quest.engine
 
     def interact(self, prompt=">>> "):
         """Starts up the REPL, prints banner message and passes user input to handle_answer."""
