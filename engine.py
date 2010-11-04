841bd23f34850d1592ea10a60cb3d7da681911a4
Gabe Berke-Williams <gbw@brandeis.edu>
2010-11-04 16:17:05 -0400

    Don't import quest.operators, since they no longer exist

--- engine.py
+++ engine.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql.expression import text
 import sqlalchemy.exc
 
-import quest.operators
 import quest.config as config
 from quest.query import query_cache
 
