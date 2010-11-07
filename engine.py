b97dd2a26202a33c6b9e6915bffa4f9ff6b096b4
Gabe Berke-Williams <gbw@brandeis.edu>
2010-11-06 22:25:20 -0400

    Update engine.py to use the new config setting names

--- engine.py
+++ engine.py
@@ -1,11 +1,14 @@
 """The actual engine that interfaces with and sends SQL code to the DB."""
 
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.sql.expression import text
 import sqlalchemy.exc
 
 import config as config
 from query import query_cache
+import util
 
 # in-memory SQLite engine
 sql_engine = create_engine('sqlite:///:memory:', echo=True)
@@ -28,14 +31,8 @@ def run_sql(sql):
 def show(query, number_of_rows = None):
     """Actually sends the given query to the database and returns the resulting
     rows as a list.
-    If number_of_rows is None, then it displays all rows in result. By default,
-    Quest does not SHOW until the user specifically commands it. The <TODO: FILL
-    ME IN> setting in config.py may be used to change the default to one of
-    three settings:
-    * SHOW_ALL: default to SHOWing every row of every command,
-    * SHOW_N: default to SHOWing N rows of every command
-              (where N = config.number_of_rows_to_show), or
-    * SHOWing only when the user asks for it (i.e. the built-in default)
+    If number_of_rows is None, then it checks config settings. For more
+    information, see the documentation in the config module.
     """
     if query is None:
         raise ValueError("query cannot be None!")
@@ -44,20 +41,20 @@ def show(query, number_of_rows = None):
     # Fetch the correct number of rows
     if number_of_rows is None:
         # Nothing specified, consult config.
-        if config.show_behavior == config.SHOW_ALL:
+        if config.ROWS_TO_SHOW == config.ALL_ROWS:
             return result.fetchall()
-        elif config.show_behavior == config.SHOW_N:
-            fetched_rows = result.fetchmany(config.number_of_rows_to_show)
-            # Since number_of_rows_to_show may be less than the total number of
-            # rows, and the ResultProxy only closes when all rows are exhausted,
-            # explicitly close the ResultProxy.
-            result.close()
-            return fetched_rows
         else:
-            # Default to showing every row
-            return result.fetchall()
+            if util.is_integer(config.ROWS_TO_SHOW):
+                fetched_rows = result.fetchmany(config.ROWS_TO_SHOW)
+                # Since number_of_rows_to_show may be less than the total number of
+                # rows, and the ResultProxy only closes when all rows are exhausted,
+                # explicitly close the ResultProxy.
+                result.close()
+                return fetched_rows
+            else:
+                raise ValueError("number_of_rows ({}) is not an int!".format(number_of_rows))
     else:
-        if str(number_of_rows).isdigit():
+        if util.is_integer(number_of_rows):
             fetched_rows = result.fetchmany(int(number_of_rows))
             # Since number_of_rows may be less than the total number of
             # rows, and the ResultProxy only closes when all rows are exhausted,
