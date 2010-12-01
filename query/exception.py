339bcad8845a6fb851a67fc35b8a0103e0458cb5
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-01 10:42:59 -0500

    Add exception.py for custom exceptions (just 1 for now)

---
+++ query/exception.py
@@ -0,0 +1,12 @@
+# Quest-specific exception classes
+
+class QuestOperationNotAllowedException(Exception):
+    """
+    Raised when user asks to run an operation and we can't do it, e.g.
+    DRILLDOWN when we can't drill down any more.
+    """
+    def __init__(self, value):
+        self.parameter = value
+   def __str__(self):
+       return repr(self.parameter)
+o
