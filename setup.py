b83e58c82df90e6c27ab82bbf5453b042bbeb76d
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 15:04:07 -0500

    Ship jQuery, too

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Quest',
     version='0.1.0',
     packages=['quest','quest.test','quest.query','quest.web'],
-    package_data={'quest.web': ['*.conf']},
+    package_data={'quest.web': ['*.conf', '*.js']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     # Can't require mysql-python :(
