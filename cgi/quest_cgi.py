dc48f9af7565a78e49ed525d8bf283136775029b
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 08:46:21 -0500

    Fix jquery path

--- cgi/quest_cgi.py
+++ cgi/quest_cgi.py
@@ -22,7 +22,7 @@ def run():
                     }
                 </style>
 
-                <script type="text/javascript" src="jquery-1.4.4..min.js"></script>
+                <script type="text/javascript" src="jquery-1.4.4.min.js"></script>
                 <script type="text/javascript">
                     $(document).ready(function(){
                         $('#theForm').submit(function(e){
