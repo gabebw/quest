5ae50a2c5d8b5146bfddc0744e7d745c4e6da96c
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 09:00:12 -0500

    Add cherrypy-based Web interface

---
+++ cgi/quest_app.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+# A simple server to run a local HTML interface to Quest 
+
+import cherrypy
+
+import os.path
+from os.path import dirname
+import sys
+
+# Used in conf file to serve static assets
+current_dir = dirname(os.path.abspath(__file__))
+dir_above_quest = dirname(dirname(current_dir))
+# So we can do "import quest"
+# Eventually, we'll actually install quest, and we won't need this hack 
+sys.path.insert(0, dir_above_quest)
+
+tutconf = os.path.join(dirname(__file__), 'quest_app.conf')
+
+import quest
+from quest import config
+from quest import input_handler
+
+quest_html = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Quest</title>
+<style type="text/css">
+    #query {
+        width: 800px;
+        height: 45px;
+    }
+    /* The div containing the REPL input and result */
+    .repl-item {
+        border-bottom: 2px solid black;
+    }
+    /* The user's input, printed back to them */
+    .repl-input {
+    }
+    /* The result of processing a user's query */
+    .repl-result {
+    }
+</style>
+
+<script type="text/javascript" src="/jquery-1.4.4.min.js"></script>
+<script type="text/javascript">
+    $.ajaxSetup({
+        url: "navigate",
+        type: "GET"
+    });
+    $(document).ready(function(){
+        $('#theForm').submit(function(e){
+            e.preventDefault();
+            var userInput = $('#query').val();
+            // third param is errorThrown for error callback
+            function onSuccessOrError(data, textStatus, xhr){
+                var replItem = $("<div/>", {
+                    "class": "repl-item",
+                    text: userInput});
+                replItem.prependTo("#mySearch");
+
+                $("<div/>", {
+                    "class": "repl-result"
+                // append so the returned value is after
+                // the user input value
+                }).html(data).appendTo(replItem);
+            }
+            $.ajax({
+                data: {query: userInput},
+                success: onSuccessOrError,
+                error: onSuccessOrError
+            });
+        });
+    });
+</script>
+</head>
+<body>
+
+<table>
+    <h1 align="center">QUEST</h1>
+    <tr>
+        <form action="navigate" id="theForm">
+        <br/>
+        <p align="center">
+            Enter your Query
+            <input id="query" name="query" size=48 />
+            <input id="submit" type="submit" value="Submit" />
+        </p>
+        </form>
+    </tr>
+
+    <tr id="mySearch">
+        <hr></hr>
+    </tr>
+</table>
+</body>
+</html>
+""".strip()
+
+class WelcomePage:
+    @cherrypy.expose
+    def index(self):
+        # Ask for the query
+        return quest_html
+
+    @cherrypy.expose
+    def navigate(self, query = None):
+        # CherryPy passes all GET and POST variables as method parameters.
+        # It doesn't make a difference where the variables come from, how
+        # large their contents are, and so on.
+        #
+        # You can define default parameter values as usual. In this
+        # example, the "name" parameter defaults to None so we can check
+        # if a name was actually specified.
+
+        if query:
+            try:
+                response = str(input_handler.handle(query)).strip()
+                # Add a period and <br> instead of newlines
+                return response.replace("\n", ".<br/>")
+            except Exception as e:
+                return "<hr><h1>Oops! Quest couldn't handle your input.</h1>"
+        else:
+            if query is None:
+                # No name was specified
+                return 'Please enter your query <a href="./">here</a>.'
+            else:
+                return 'No, really, enter your query <a href="./">here</a>.'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print "Usage: %s db_name db_password" % __file__
+    else:
+        config.db_name = sys.argv[1]
+        config.db_password = sys.argv[2]
+
+        # CherryPy always starts with app.root when trying to map request URIs
+        # to objects, so we need to mount a request handler root. A request
+        # to '/' will be mapped to HelloWorld().index().
+        cherrypy.quickstart(WelcomePage(), config=tutconf)
+else:
+    # This branch is for the test suite; you can ignore it.
+    cherrypy.tree.mount(WelcomePage(), config=tutconf)
