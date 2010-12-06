6d2f66aa9cd140f56a54441a8c11ebb2774b6b03
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 07:39:44 -0500

    Remove unneeded navbar styles for quest_cgi

--- cgi/quest_cgi.py
+++ cgi/quest_cgi.py
@@ -16,27 +16,6 @@ def run():
                 <head>
                 <title>Quest</title>
                 <style type="text/css">
-                #navbar ul {
-                    margin: 0;
-                    padding: 5px;
-                    list-style-type: none;
-                    text-align: center;
-                    background-color: #000;
-                }
-
-                #navbar ul li { display: inline; }
-
-                #navbar ul li a {
-                    text-decoration: none;
-                    padding: .2em 1em;
-                    color: #fff;
-                    background-color: #000;
-                }
-
-                #navbar ul li a:hover {
-                    color: #000;
-                    background-color: #fff;
-                }
                 </style>
 
                 <script type="text/javascript" src="jquery-1.4.4..min.js"></script>
