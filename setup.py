41e03098aea8203201a57ce8919e3b0236da5de3
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 16:01:02 -0500

    Add a homepage

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=['quest','quest.test','quest.query','quest.web'],
     package_data={'quest.web': ['*.conf', '*.js']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    homepage="https://github.com/gabebw/quest",
     long_description=open('README.txt').read(),
     # Can't require mysql-python :(
     requires=['sqlalchemy (>=0.6.5)', 'sqlparse', 'cherrypy (>=3.1)']
