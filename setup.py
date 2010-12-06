a3c603effe1b41ca8ced371f92d839aaf9492272
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 12:25:53 -0500

    Install all packages, not just top level

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Quest',
     version='0.1.0',
-    packages=['quest',],
+    packages=['quest','quest.test','quest.query','quest.web'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     # Can't require mysql-python :(
