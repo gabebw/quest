744fd5f7f7170b7f4143b5229db5fe4f99422096
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-06 14:58:52 -0500

    Include conf file for cherrypy in distribution

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='Quest',
     version='0.1.0',
     packages=['quest','quest.test','quest.query','quest.web'],
+    package_data={'quest.web': ['*.conf']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     # Can't require mysql-python :(
