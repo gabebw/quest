0a2fbc338ad83e8069b0839761034d245f6cb37b
Gabe Berke-Williams <gbw@brandeis.edu>
2010-12-21 21:48:22 -0800

    Add scripts so they're installed in bin

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setup(
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
     # Can't require mysql-python :(
-    requires=['sqlalchemy (>=0.6.5)', 'sqlparse', 'cherrypy (>=3.1)']
+    requires=['sqlalchemy (>=0.6.5)', 'sqlparse', 'cherrypy (>=3.1)'],
+    scripts=['quest/test/repl-test.py',
+        'quest/test/web-test.py',
+        'quest/test/createDB.py']
 )
